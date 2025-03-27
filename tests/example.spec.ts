cc7aa3d22c22a84c91f0afd4dea2d201751ab312
bkita <2436425+bkita@users.noreply.github.com>
2025-03-27 11:19:19 +0100

    [BSAPI L03]:feat - delete example test

--- tests/example.spec.ts
+++
@@ -1,6 +0,0 @@
-import { expect, test } from '@playwright/test';
-
-test('should return status code 200 ok', async ({ request }) => {
-  const response = await request.get('https://jsonplaceholder.typicode.com/users');
-  expect(response.status()).toBe(200);
-});
