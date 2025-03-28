6351e0d8dd78cdcb24a8160be372273ee281e2c6
bkita <2436425+bkita@users.noreply.github.com>
2025-03-28 10:05:55 +0100

    [BSAPI L12]:feat - refactor url builder

--- src/api/helpers/url.helper.ts
+++ src/api/helpers/url.helper.ts
@@ -1,9 +1,25 @@
 import { API_AUTHORS } from '@const/endpoints.const';
 
-export function buildResourceUrl(endpoint: string, id?: number): string {
-  return id ? `${endpoint}/${String(id)}` : endpoint;
+type QueryParams = Record<string, string | number | boolean>;
+
+export function buildResourceUrl(endpoint: string, idOrParams?: number | QueryParams, params?: QueryParams): string {
+  let url = endpoint;
+
+  if (typeof idOrParams === 'number') {
+    url += `/${String(idOrParams)}`;
+  } else {
+    params = idOrParams;
+  }
+
+  if (params && Object.keys(params).length > 0) {
+    const stringParams = Object.fromEntries(Object.entries(params).map(([key, value]) => [key, String(value)]));
+    const queryString = new URLSearchParams(stringParams).toString();
+    url += `?${queryString}`;
+  }
+
+  return url;
 }
 
-export function authorsUrl(id?: number): string {
-  return buildResourceUrl(API_AUTHORS, id);
+export function authorsUrl(idOrParams?: number | QueryParams, params?: QueryParams): string {
+  return buildResourceUrl(API_AUTHORS, idOrParams, params);
 }
--- tests/authors/authors.get.search.spec.ts
+++ tests/authors/authors.get.search.spec.ts
@@ -1,84 +1,73 @@
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-
+import { AuthorResponse } from '@api-models/authors/authors.model';
+import { HTTP_200_OK } from '@const/http.status.codes.const';
 import { expect, test } from '@fixtures/api.fixture';
+import { parseResponse } from '@helpers/parse.response.helper';
+import { statusCode } from '@helpers/response.status.helper';
+import { authorsUrl } from '@helpers/url.helper';
+import { getRequest } from '@requests/get.request';
 
 const EXPECTED_FIRST_NAME = 'Joshua';
 const EXPECTED_LAST_NAME = 'Bloch';
 const NON_EXISTING_NAME = 'XXXXXX';
 
 test.describe('GET /authors 2xx', { tag: ['@authors', '@smoke', '@search'] }, () => {
-  test('search by firstName', async ({ request }) => {
-    const response = await request.get('/authors', {
-      params: {
-        firstName: EXPECTED_FIRST_NAME,
-      },
-    });
-    expect(response.status()).toBe(200);
+  test('search by firstName', async () => {
+    const queryParams = { firstName: EXPECTED_FIRST_NAME };
 
-    const authors = await response.json();
+    const response = await getRequest(authorsUrl(queryParams));
+    expect(statusCode(response)).toBe(HTTP_200_OK);
+
+    const authors = await parseResponse<AuthorResponse[]>(response);
     expect(authors.length).toBeGreaterThanOrEqual(1);
   });
 
-  test('search by non existing firstName', async ({ request }) => {
-    const response = await request.get('/authors', {
-      params: {
-        firstName: NON_EXISTING_NAME,
-      },
-    });
-    expect(response.status()).toBe(200);
+  test('search by non existing firstName', async () => {
+    const queryParams = { firstName: NON_EXISTING_NAME };
 
-    const authors = await response.json();
+    const response = await getRequest(authorsUrl(queryParams));
+    expect(statusCode(response)).toBe(HTTP_200_OK);
+
+    const authors = await parseResponse<AuthorResponse[]>(response);
     expect(authors.length).toBe(0);
   });
 
-  test('search by lastName', async ({ request }) => {
-    const response = await request.get('/authors', {
-      params: {
-        lastName: EXPECTED_LAST_NAME,
-      },
-    });
-    expect(response.status()).toBe(200);
+  test('search by lastName', async () => {
+    const queryParams = { lastName: EXPECTED_LAST_NAME };
 
-    const authors = await response.json();
+    const response = await getRequest(authorsUrl(queryParams));
+    expect(statusCode(response)).toBe(HTTP_200_OK);
+
+    const authors = await parseResponse<AuthorResponse[]>(response);
     expect(authors.length).toBeGreaterThanOrEqual(1);
   });
 
-  test('search by non existing lastName', async ({ request }) => {
-    const response = await request.get('/authors', {
-      params: {
-        lastName: NON_EXISTING_NAME,
-      },
-    });
-    expect(response.status()).toBe(200);
+  test('search by non existing lastName', async () => {
+    const queryParams = { lastName: NON_EXISTING_NAME };
 
-    const authors = await response.json();
+    const response = await getRequest(authorsUrl(queryParams));
+    expect(statusCode(response)).toBe(HTTP_200_OK);
+
+    const authors = await parseResponse<AuthorResponse[]>(response);
     expect(authors.length).toBe(0);
   });
 
-  test('search by firstName and lastName', async ({ request }) => {
-    const response = await request.get('/authors', {
-      params: {
-        firstName: EXPECTED_FIRST_NAME,
-        lastName: EXPECTED_LAST_NAME,
-      },
-    });
-    expect(response.status()).toBe(200);
+  test('search by firstName and lastName', async () => {
+    const queryParams = { firstName: EXPECTED_FIRST_NAME, lastName: EXPECTED_LAST_NAME };
 
-    const authors = await response.json();
+    const response = await getRequest(authorsUrl(queryParams));
+    expect(statusCode(response)).toBe(HTTP_200_OK);
+
+    const authors = await parseResponse<AuthorResponse[]>(response);
     expect(authors.length).toBeGreaterThanOrEqual(1);
   });
 
-  test('search by non existing firstName and non existing lastName', async ({ request }) => {
-    const response = await request.get('/authors', {
-      params: {
-        firstName: NON_EXISTING_NAME,
-        lastName: NON_EXISTING_NAME,
-      },
-    });
-    expect(response.status()).toBe(200);
+  test('search by non existing firstName and non existing lastName', async () => {
+    const queryParams = { firstName: NON_EXISTING_NAME, lastName: NON_EXISTING_NAME };
 
-    const authors = await response.json();
+    const response = await getRequest(authorsUrl(queryParams));
+    expect(statusCode(response)).toBe(HTTP_200_OK);
+
+    const authors = await parseResponse<AuthorResponse[]>(response);
     expect(authors.length).toBe(0);
   });
 });
