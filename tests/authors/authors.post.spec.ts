731d17910b9c037711aa74529af02939bbfb808a
bkita <2436425+bkita@users.noreply.github.com>
2025-03-30 12:32:12 +0200

    [BSAPI L20]:feat - practice solution

--- tests/authors/authors.post.spec.ts
+++ tests/authors/authors.post.spec.ts
@@ -1,5 +1,6 @@
 import { AuthorResponse } from '@api-models/authors/authors.model';
 import { HTTP_201_CREATED } from '@const/http.status.codes.const';
+import { MAX_LENGTH, MIN_LENGTH } from '@const/validation.const';
 import { getRandomAuthorPayload } from '@datafactory/authors/author.data';
 import { expect, test } from '@fixtures/api.fixture';
 import { parseResponse } from '@helpers/parse.response.helper';
@@ -8,6 +9,21 @@ import { authorsUrl } from '@helpers/url.helper';
 import { postRequest } from '@requests/post.request';
 import { deleteAuthorAPIStep } from 'src/api/steps/authors/delete.author.step';
 
+const validAuthorPayload = [
+  {
+    authorPayload: getRandomAuthorPayload(),
+    description: 'valid data',
+  },
+  {
+    authorPayload: { firstName: 'a'.repeat(MIN_LENGTH), lastName: 'a'.repeat(MIN_LENGTH) },
+    description: 'min length values',
+  },
+  {
+    authorPayload: { firstName: 'a'.repeat(MAX_LENGTH), lastName: 'a'.repeat(MAX_LENGTH) },
+    description: 'max length values',
+  },
+];
+
 test.describe('POST /authors 2xx', { tag: ['@authors', '@smoke'] }, () => {
   let authorId: number;
 
@@ -15,15 +31,15 @@ test.describe('POST /authors 2xx', { tag: ['@authors', '@smoke'] }, () => {
     if (authorId) await deleteAuthorAPIStep(authorId);
   });
 
-  test('create new author', async () => {
-    const authorPayload = getRandomAuthorPayload();
+  validAuthorPayload.forEach(({ authorPayload, description }) => {
+    test(`author: ${description}`, async () => {
+      const response = await postRequest(authorsUrl(), authorPayload);
+      expect(statusCode(response)).toBe(HTTP_201_CREATED);
 
-    const response = await postRequest(authorsUrl(), authorPayload);
-    expect(statusCode(response)).toBe(HTTP_201_CREATED);
-
-    const responseBody = await parseResponse<AuthorResponse>(response);
-    authorId = responseBody.id;
-    expect(responseBody.firstName).toBe(authorPayload.firstName);
-    expect(responseBody.lastName).toBe(authorPayload.lastName);
+      const responseBody = await parseResponse<AuthorResponse>(response);
+      authorId = responseBody.id;
+      expect(responseBody.firstName).toBe(authorPayload.firstName);
+      expect(responseBody.lastName).toBe(authorPayload.lastName);
+    });
   });
 });
