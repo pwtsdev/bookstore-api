3ad25eef2fbc2e728ccf7fe14e570ba1357043e9
bkita <2436425+bkita@users.noreply.github.com>
2025-04-02 14:20:38 +0200

    [BSAPI M14L02]:feat - cleanup after creating a book

--- tests/books/books.post.spec.ts
+++ tests/books/books.post.spec.ts
@@ -8,11 +8,14 @@ import { statusCode } from '@helpers/response.status.helper';
 import { booksUrl } from '@helpers/url.helper';
 import { postRequest } from '@requests/post.request';
 import { createAuthorAPIStep } from 'src/api/steps/authors/create.author.step';
+import { deleteAuthorAPIStep } from 'src/api/steps/authors/delete.author.step';
+import { deleteBookAPIStep } from 'src/api/steps/books/delete.book.step';
 
 test.describe('POST /books 2xx', { tag: ['@books', '@smoke'] }, () => {
   let author: AuthorResponse;
   let authorId: number;
   let bookPayload: BookPayload;
+  let bookId: number;
 
   test.beforeEach('create test data', async () => {
     author = await createAuthorAPIStep();
@@ -21,15 +24,18 @@ test.describe('POST /books 2xx', { tag: ['@books', '@smoke'] }, () => {
     bookPayload = getRandomBookPayload(authorId);
   });
 
-  // test.afterEach(async () => {
-  //   if (authorId) await deleteAuthorAPIStep(authorId);
-  // });
+  test.afterEach(async () => {
+    if (bookId) await deleteBookAPIStep(bookId);
+    if (authorId) await deleteAuthorAPIStep(authorId);
+  });
 
   test('create a new book', async () => {
     const response = await postRequest(booksUrl(), bookPayload);
     expect(statusCode(response)).toBe(HTTP_201_CREATED);
 
     const responseBody = await parseResponse<BookResponse>(response);
+    bookId = responseBody.id;
+
     expect(responseBody).toMatchObject({
       title: bookPayload.title,
       year: bookPayload.year,
