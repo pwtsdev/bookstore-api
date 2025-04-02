01bca1ab80fe51387ccbb6f282704177e846f21b
bkita <2436425+bkita@users.noreply.github.com>
2025-04-02 12:44:00 +0200

    [BSAPI M13L16]:test - create new book - part II

--- tests/books/books.post.spec.ts
+++ tests/books/books.post.spec.ts
@@ -1,4 +1,5 @@
-import { BookResponse } from '@api-models/books/book.model';
+import { AuthorResponse } from '@api-models/authors/author.model';
+import { BookPayload, BookResponse } from '@api-models/books/book.model';
 import { HTTP_201_CREATED } from '@const/http.status.codes.const';
 import { expect, test } from '@fixtures/api.fixture';
 import { parseResponse } from '@helpers/parse.response.helper';
@@ -6,17 +7,31 @@ import { getRandomBookTitle } from '@helpers/random.data.helper';
 import { statusCode } from '@helpers/response.status.helper';
 import { booksUrl } from '@helpers/url.helper';
 import { postRequest } from '@requests/post.request';
+import { createAuthorAPIStep } from 'src/api/steps/authors/create.author.step';
 
 test.describe('POST /books 2xx', { tag: ['@books', '@smoke'] }, () => {
-  test('create a new book', async () => {
-    const bookPayload = {
+  let author: AuthorResponse;
+  let authorId: number;
+  let bookPayload: BookPayload;
+
+  test.beforeEach('create test data', async () => {
+    author = await createAuthorAPIStep();
+    authorId = author.id;
+
+    bookPayload = {
       title: getRandomBookTitle(),
-      authors: [1],
+      authors: [authorId],
       year: 2022,
       price: 49.99,
       available: 100,
     };
+  });
 
+  // test.afterEach(async () => {
+  //   if (authorId) await deleteAuthorAPIStep(authorId);
+  // });
+
+  test('create a new book', async () => {
     const response = await postRequest(booksUrl(), bookPayload);
     expect(statusCode(response)).toBe(HTTP_201_CREATED);
 
@@ -26,6 +41,7 @@ test.describe('POST /books 2xx', { tag: ['@books', '@smoke'] }, () => {
       year: bookPayload.year,
       price: bookPayload.price,
       available: bookPayload.available,
+      authors: [author],
     });
   });
 });
