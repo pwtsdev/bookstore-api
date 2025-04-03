8e18f896accef1c76d4c3e7ea06c0445c111e6fe
bkita <2436425+bkita@users.noreply.github.com>
2025-04-03 10:06:37 +0200

    [BSAPI M14L04]:test - partial update book

--- tests/books/books.patch.spec.ts
+++ tests/books/books.patch.spec.ts
@@ -1,22 +1,18 @@
-import { AuthorResponse } from '@api-models/authors/author.model';
 import { BookPayload, BookResponse } from '@api-models/books/book.model';
 import { HTTP_200_OK } from '@const/http.status.codes.const';
-import { getRandomBookPayload } from '@datafactory/books/book.data';
 import { expect, test } from '@fixtures/api.fixture';
 import { parseResponse } from '@helpers/parse.response.helper';
 import { getRandomBookTitle } from '@helpers/random.data.helper';
 import { statusCode } from '@helpers/response.status.helper';
 import { booksUrl } from '@helpers/url.helper';
-import { putRequest } from '@requests/put.request';
-import { createAuthorAPIStep } from 'src/api/steps/authors/create.author.step';
+import { patchRequest } from '@requests/patch.request';
 import { deleteAuthorAPIStep } from 'src/api/steps/authors/delete.author.step';
 import { createBookAPIStep } from 'src/api/steps/books/create.book.step';
 import { deleteBookAPIStep } from 'src/api/steps/books/delete.book.step';
 
-test.describe('PUT /books 2xx', { tag: ['@books', '@smoke'] }, () => {
+test.describe('PATCH /books 2xx', { tag: ['@books', '@smoke'] }, () => {
   let existingBook: BookResponse;
-  let author: AuthorResponse;
-  let bookPayload: BookPayload;
+  let randomBookTitle: string;
   let bookId: number;
   let authorId: number;
 
@@ -26,13 +22,7 @@ test.describe('PUT /books 2xx', { tag: ['@books', '@smoke'] }, () => {
     bookId = existingBook.id;
     authorId = existingBook.authors[0].id;
 
-    // BOOK PAYLOAD
-    author = await createAuthorAPIStep();
-    bookPayload = getRandomBookPayload(author.id);
-    bookPayload.title = getRandomBookTitle();
-    bookPayload.year = 2000;
-    bookPayload.price = 9.99;
-    bookPayload.available = 500;
+    randomBookTitle = getRandomBookTitle();
   });
 
   test.afterEach(async () => {
@@ -40,18 +30,22 @@ test.describe('PUT /books 2xx', { tag: ['@books', '@smoke'] }, () => {
     if (authorId) await deleteAuthorAPIStep(authorId);
   });
 
-  test('update existing book', async () => {
-    const response = await putRequest(booksUrl(existingBook.id), bookPayload);
+  test('update book title', async () => {
+    const bookPayload = { title: randomBookTitle } as BookPayload;
+    const response = await patchRequest(booksUrl(existingBook.id), bookPayload);
     expect(statusCode(response)).toBe(HTTP_200_OK);
 
     const responseBody = await parseResponse<BookResponse>(response);
-    expect(responseBody).toMatchObject({
-      id: existingBook.id,
-      title: bookPayload.title,
-      year: bookPayload.year,
-      price: bookPayload.price,
-      available: bookPayload.available,
-      authors: [author],
-    });
+    existingBook.title = randomBookTitle;
+    expect(responseBody).toMatchObject(existingBook);
+  });
+
+  test('update with empty json', async () => {
+    const bookPayload = {} as BookPayload;
+    const response = await patchRequest(booksUrl(existingBook.id), bookPayload);
+    expect(statusCode(response)).toBe(HTTP_200_OK);
+
+    const responseBody = await parseResponse<BookResponse>(response);
+    expect(responseBody).toMatchObject(existingBook);
   });
 });
